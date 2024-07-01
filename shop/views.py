471e0eed2e9b904894191189a847e7394356d7cb
Aswini Kumar <aswini0509@gmail.com>
2024-07-02 02:29:09 +0530

    index checkout

--- shop/views.py
+++ shop/views.py
@@ -5,19 +5,19 @@ from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
-    Product_object = Product.objects.all()
+    product_object = Product.objects.all()
 
     # Search Functionality
     item_name = request.GET.get('item_name')
     if item_name!='' and item_name is not None:
-        Product_object = Product_object.filter(title__icontains=item_name)
+        product_object = product_object.filter(title__icontains=item_name)
     
 
-    paginator = Paginator(Product_object,5)
+    paginator = Paginator(product_object,4)
     page = request.GET.get('page')
-    Product_object = paginator.get_page(page)
+    product_object = paginator.get_page(page)
     
-    return render(request , 'shop/index.html',context={'product_object':Product_object})
+    return render(request , 'shop/index.html',context={'product_object':product_object})
 
 
 def detail(request,id):
